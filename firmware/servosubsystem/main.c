bc806a80481c4d7bca15d1ca3f3ca1389891c865
Sharmaa-amit <71359389+Sharmaa-amit@users.noreply.github.com>
2021-03-26 11:01:16 -0400

    Update main.c

--- firmware/servosubsystem/main.c
+++ firmware/servosubsystem/main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Warren Gay   Sat Dec 30 11:59:39 2017
  * Modified by: Amit Sharma 
  */
@@ -13,27 +13,28 @@
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/cm3/nvic.h>
 
-static void task1(void *args __attribute__((unused))) {
+#include "usbcdc.h"
 
-	static const int ms[4] = { 1375, 1725, 2100, 1725 };
-	int msx = 0;
+static void gate(void *args __attribute__((unused))) {
 
-	gpio_set(GPIOC, GPIO13);			// LED on
+	char state;
 
-	rcc_periph_clock_enable(RCC_TIM2);		// Need TIM2 clock
-	rcc_periph_clock_enable(RCC_AFIO);		// Need AFIO clock
+	rcc_periph_clock_enable(RCC_TIM2); // Need TIM2 clock
+	rcc_periph_clock_enable(RCC_AFIO); // Need AFIO clock
 
 	// PA1 == TIM2.CH2	
-	rcc_periph_clock_enable(RCC_GPIOA);		// Need GPIOA clock	
-
+	rcc_periph_clock_enable(RCC_GPIOA); // Need GPIOA clock	
+	rcc_periph_clock_enable(RCC_GPIOC);
+	
 	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO1); // High speed, GPIOA1 = TIM2.CH2
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 
 	// TIM2:
 	timer_disable_counter(TIM2);
 	rcc_periph_reset_pulse(RST_TIM2);
 
 	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-	timer_set_prescaler(TIM2, 72-1); //see https://github.com/ve3wwg/stm32f103c8t6/pull/12/
+	timer_set_prescaler(TIM2, 72-1); // See https://github.com/ve3wwg/stm32f103c8t6/pull/12/
 
 	timer_enable_preload(TIM2);
 	timer_continuous_mode(TIM2);
@@ -43,15 +44,27 @@ static void task1(void *args __attribute__((unused))) {
 	timer_set_oc_mode(TIM2, TIM_OC2, TIM_OCM_PWM1);
 	timer_enable_oc_output(TIM2, TIM_OC2);
 
-	timer_set_oc_value(TIM2,TIM_OC2, ms[msx=0]);
+	timer_set_oc_value(TIM2, TIM_OC2, 1725);
+	gpio_set(GPIOC, GPIO13);
+
 	timer_enable_counter(TIM2);
 
 	for (;;)
 	{
-		vTaskDelay(1000);
-		gpio_toggle(GPIOC, GPIO13);
-		msx = (msx+1) % 4;
-		timer_set_oc_value(TIM2, TIM_OC2, ms[msx]);
+		state = usb_getc();
+		
+		if (state == 'o') // Open
+		{
+			timer_set_oc_value(TIM2, TIM_OC2, 850);
+			gpio_clear(GPIOC, GPIO13);
+		}
+
+
+		if (state == 'c') // Close
+		{
+			timer_set_oc_value(TIM2, TIM_OC2, 1725);
+			gpio_set(GPIOC, GPIO13);
+		}
 	}
 }
 
@@ -59,12 +72,9 @@ int main(void) {
 
 	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
-	rcc_periph_clock_enable(RCC_GPIOC);
-	
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
-	gpio_clear(GPIOC, GPIO13);			// LED off
+	usb_start();
 
-	xTaskCreate(task1, "task1", 100, NULL, 1, NULL);
+	xTaskCreate(gate, "gate", 100, NULL, configMAX_PRIORITIES-1, NULL);
 	vTaskStartScheduler();
 	
 	for (;;);
